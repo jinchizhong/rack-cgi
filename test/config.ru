eae211d993e810a26da73eebdd0fea455a691736
jinchizhong <jinchizhong@kingsoft.com>
2015-05-17 15:58:24 +0800

    add unittest

--- test/config.ru
+++ test/config.ru
@@ -1,5 +1,5 @@
 require '../lib/rack-cgi'
 
-use Rack::CGI, cgi_path: 'cgi', index: 'index.cgi', Rack::CGI::Executable => '', /\.php$/ => '/usr/bin/php5-cgi'
+use Rack::CGI, cgi_path: 'cgi', index: 'index.cgi', Rack::CGI::Executable => '', /\.php$/ => '/usr/bin/php-cgi'
 use Rack::Static, urls: ['/'], root: 'cgi'
 run proc{ [404, {"CONTENT-TYPE" => "text/plain"}, ['404 Not Found']] }
---
+++ test/test.rb
@@ -0,0 +1,72 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'test/unit'
+require 'rack'
+require 'rack/test'
+
+class RackCgiTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    s, c = Rack::Builder.parse_file "./config.ru"
+    s
+  end
+
+  def res
+    last_response
+  end
+
+  def test_cgi_index
+    get '/'
+    assert res.ok?
+    assert_equal res.body, "Hello world\n"
+    assert_equal res.headers['Content-Type'], 'text/plain'
+  end
+
+  def test_cgi_error
+    get '/error'
+    assert !res.ok?
+    assert_equal res.status, 500
+    assert_equal res.body, "500 Server Internal Error\n"
+  end
+
+  def test_cgi_export
+    get '/export'
+    assert res.ok?
+    
+    body = res.body
+    h = {}
+    body.lines.each do |line|
+      line = line.sub /^export /, ''
+      k, v = line.split '='
+      h[k] = v
+    end
+    assert_equal h['SCRIPT_FILENAME'], "'" + Dir.pwd + "/cgi/export'\n"
+  end
+
+  def test_cgi_file
+    get '/file'
+    assert res.ok?
+
+    assert_equal res.body, "This is a test file for test\n"
+  end
+
+  def test_cgi_redirect
+    get '/redirect'
+    assert_equal res.status, 302
+    assert_equal res.headers['Location'], 'http://www.google.com'
+  end
+
+  def test_cgi_php
+    if !File.exists? '/usr/bin/php-cgi'
+      puts 'php-cgi not found, skip php-cgi test!'
+    end
+    get '/test.php'
+    assert res.ok?
+  end
+  
+  def test_without_header
+    get '/without_header'
+    assert_equal res.body, "Hello world\n"
+  end
+end
